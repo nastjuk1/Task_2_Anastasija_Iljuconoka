464660898dd334ba1fa2240256cb18a07d1ab2d3
nastjuk1 <123481485+nastjuk1@users.noreply.github.com>
2023-05-10 23:25:45 +0300

    Add files via upload

---
+++ assets/js/theme.js
@@ -0,0 +1,15 @@
+﻿const form = document.querySelector('form'); 
+const nameInput = document.getElementById('name-input'); 
+const emailInput = document.getElementById('email-input'); 
+const phoneInput = document.getElementById('phone-input'); 
+const commentInput = document.getElementById('comment'); 
+const checkbox = document.querySelector('.check_box'); 
+
+form.addEventListener('submit', function (event) {
+    if (!nameInput.value || !emailInput.value || !phoneInput.value || !commentInput.value || !checkbox.checked) {
+        alert('Kļūda');
+        event.preventDefault(); 
+    }
+});
+
+
